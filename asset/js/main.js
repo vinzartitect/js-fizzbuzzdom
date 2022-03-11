7a84625f27f8e9da819569a164ec110540ea4ad8
VincenzoBorriello89 <vincborr89@gmail.com>
2022-03-11 16:34:16 +0100

    creazione file e primo step

---
+++ asset/js/main.js
@@ -0,0 +1,22 @@
+// milestone 1 script di prova in console log 
+
+for (let i = 0; i < 100; i++) {
+
+    if (i % 3 == 0) {
+
+         console.log("Fizz");
+
+    } else if (i % 5 == 0) {
+
+        console.log("Buzz");
+
+    } if (i % 3 == 0 && i % 5 == 0) {
+
+        console.log("FizzBuzz");
+
+    } else {
+
+        console.log(i);
+
+    }
+};
